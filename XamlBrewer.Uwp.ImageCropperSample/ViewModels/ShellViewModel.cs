281e2682e418ad7e69d88c56c676fd028955d360
Diederik Krols <diederik.krols@u2uconsult.com>
2015-08-06 22:22:52 +0200

    CameraPage.

--- XamlBrewer.Uwp.ImageCropperSample/ViewModels/ShellViewModel.cs
+++ XamlBrewer.Uwp.ImageCropperSample/ViewModels/ShellViewModel.cs
@@ -16,6 +16,7 @@ namespace Mvvm
             // Build the menu
             Menu.Add(new MenuItem() { Glyph = "\uEA3A", Text = "Native", NavigationDestination = typeof(MainPage) });
             Menu.Add(new MenuItem() { Glyph = "\uE2B1", Text = "Templated", NavigationDestination = typeof(TemplatePage) });
+            Menu.Add(new MenuItem() { Glyph = "\uE114", Text = "Camera", NavigationDestination = typeof(CameraPage) });
         }
     }
 }
