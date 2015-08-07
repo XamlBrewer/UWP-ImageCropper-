d78d3e1200ff2eb6561ee48436a5059505bc066e
Diederik Krols <diederik.krols@u2uconsult.com>
2015-08-07 20:22:57 +0200

    Camera as a Service.

---
+++ XamlBrewer.Uwp.ImageCropperSample/Services/CameraService.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Graphics.Display;
+using Windows.Media.Capture;
+using Windows.System.Display;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Mvvm.Services
+{
+    public class CameraService
+    {
+        private CaptureElement _captureElement;
+
+        // Receive notifications about rotation of the UI and apply any necessary rotation to the preview stream
+        private readonly DisplayInformation _displayInformation = DisplayInformation.GetForCurrentView();
+        private DisplayOrientations _displayOrientation = DisplayOrientations.Portrait;
+
+        // Prevent the screen from sleeping while the camera is running
+        private readonly DisplayRequest _displayRequest = new DisplayRequest();
+
+        // MediaCapture and its state variables
+        private MediaCapture _mediaCapture = null;
+        private bool _isInitialized = false;
+        private bool _isPreviewing = false;
+
+        // Information about the camera device
+        private bool _mirroringPreview = false;
+        private bool _externalCamera = false;
+
+        // Rotation metadata to apply to the preview stream (MF_MT_VIDEO_ROTATION)
+        // Reference: http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh868174.aspx
+        private static readonly Guid RotationKey = new Guid("C380465D-2271-428C-9B83-ECEA3B4A85C1");
+
+        public CameraService(CaptureElement el)
+        {
+            _captureElement = el;
+        }
+
+        internal MediaCapture MediaCapture
+        {
+            get
+            {
+                return _mediaCapture;
+            }
+        }
+
+        internal async Task InitializeCameraAsync()
+        {
+            Debug.WriteLine("InitializeCameraAsync");
+
+            if (_mediaCapture == null)
+            {
+                // Attempt to get the back camera if one is available, but use any camera device if not
+                var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Back);
+
+                if (cameraDevice == null)
+                {
+                    Debug.WriteLine("No camera device found!");
+                    return;
+                }
+
+                // Create MediaCapture and its settings
+                _mediaCapture = new MediaCapture();
+
+                // Register for a notification when something goes wrong
+                _mediaCapture.Failed += MediaCapture_Failed;
+
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                // Initialize MediaCapture
+                try
+                {
+                    await _mediaCapture.InitializeAsync(settings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Debug.WriteLine("The app was denied access to the camera");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Exception when initializing MediaCapture with {0}: {1}", cameraDevice.Id, ex.ToString());
+                }
+
+                // If initialization succeeded, start the preview
+                if (_isInitialized)
+                {
+                    // Figure out where the camera is located
+                    if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Unknown)
+                    {
+                        // No information on the location of the camera, assume it's an external camera, not integrated on the device
+                        _externalCamera = true;
+                    }
+                    else
+                    {
+                        // Camera is fixed on the device
+                        _externalCamera = false;
+
+                        // Only mirror the preview if the camera is on the front panel
+                        _mirroringPreview = (cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
+                    }
+
+                    await StartPreviewAsync();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the current orientation of the UI in relation to the device and applies a corrective rotation to the preview
+        /// </summary>
+        internal async Task SetPreviewRotationAsync()
+        {
+            // Only need to update the orientation if the camera is mounted on the device
+            if (_externalCamera) return;
+
+            // Calculate which way and how far to rotate the preview
+            int rotationDegrees = ConvertDisplayOrientationToDegrees(_displayOrientation);
+
+            // The rotation direction needs to be inverted if the preview is being mirrored
+            if (_mirroringPreview)
+            {
+                rotationDegrees = (360 - rotationDegrees) % 360;
+            }
+
+            // Add rotation metadata to the preview stream to make sure the aspect ratio / dimensions match when rendering and getting preview frames
+            var props = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview);
+            props.Properties.Add(RotationKey, rotationDegrees);
+            await _mediaCapture.SetEncodingPropertiesAsync(MediaStreamType.VideoPreview, props, null);
+        }
+
+        /// <summary>
+        /// Starts the preview and adjusts it for for rotation and mirroring after making a request to keep the screen on and unlocks the UI
+        /// </summary>
+        /// <returns></returns>
+        internal async Task StartPreviewAsync()
+        {
+            Debug.WriteLine("StartPreviewAsync");
+
+            // Prevent the device from sleeping while the preview is running
+            _displayRequest.RequestActive();
+
+            // Set the preview source in the UI and mirror it if necessary
+            _captureElement.Source = _mediaCapture;
+            _captureElement.FlowDirection = _mirroringPreview ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+
+            // Start the preview
+            try
+            {
+                await _mediaCapture.StartPreviewAsync();
+                _isPreviewing = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception when starting the preview: {0}", ex.ToString());
+            }
+
+            // Initialize the preview to the current orientation
+            if (_isPreviewing)
+            {
+                await SetPreviewRotationAsync();
+            }
+
+            // Enable / disable the button depending on the preview state
+            //GetPreviewFrameButton.IsEnabled = _isPreviewing;
+        }
+
+        /// <summary>
+        /// Queries the available video capture devices to try and find one mounted on the desired panel
+        /// </summary>
+        /// <param name="desiredPanel">The panel on the device that the desired camera is mounted on</param>
+        /// <returns>A DeviceInformation instance with a reference to the camera mounted on the desired panel if available,
+        ///          any other camera if not, or null if no camera is available.</returns>
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            // Get the desired camera by panel
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+
+            // If there is no device mounted on the desired panel, return the first device found
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
+        }
+
+        private async void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            Debug.WriteLine("MediaCapture_Failed: (0x{0:X}) {1}", errorEventArgs.Code, errorEventArgs.Message);
+
+            await CleanupCameraAsync();
+
+            // await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => GetPreviewFrameButton.IsEnabled = _isPreviewing);
+        }
+
+        /// <summary>
+        /// Cleans up the camera resources (after stopping the preview if necessary) and unregisters from MediaCapture events
+        /// </summary>
+        /// <returns></returns>
+        private async Task CleanupCameraAsync()
+        {
+            if (_isInitialized)
+            {
+                if (_isPreviewing)
+                {
+                    // The call to stop the preview is included here for completeness, but can be
+                    // safely removed if a call to MediaCapture.Dispose() is being made later,
+                    // as the preview will be automatically stopped at that point
+                    await StopPreviewAsync();
+                }
+
+                _isInitialized = false;
+            }
+
+            if (_mediaCapture != null)
+            {
+                _mediaCapture.Failed -= MediaCapture_Failed;
+                _mediaCapture.Dispose();
+                _mediaCapture = null;
+            }
+        }
+
+        /// <summary>
+        /// Stops the preview and deactivates a display request, to allow the screen to go into power saving modes, and locks the UI
+        /// </summary>
+        /// <returns></returns>
+        internal async Task StopPreviewAsync()
+        {
+            try
+            {
+                _isPreviewing = false;
+                await _mediaCapture.StopPreviewAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception when stopping the preview: {0}", ex.ToString());
+            }
+
+            // Use the dispatcher because this method is sometimes called from non-UI threads
+            await _captureElement.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                _captureElement.Source = null;
+
+                // Allow the device to sleep now that the preview is stopped
+                _displayRequest.RequestRelease();
+
+                //GetPreviewFrameButton.IsEnabled = _isPreviewing;
+            });
+        }
+
+        /// <summary>
+        /// Converts the given orientation of the app on the screen to the corresponding rotation in degrees
+        /// </summary>
+        /// <param name="orientation">The orientation of the app on the screen</param>
+        /// <returns>An orientation in degrees</returns>
+        private static int ConvertDisplayOrientationToDegrees(DisplayOrientations orientation)
+        {
+            switch (orientation)
+            {
+                case DisplayOrientations.Portrait:
+                    return 90;
+                case DisplayOrientations.LandscapeFlipped:
+                    return 180;
+                case DisplayOrientations.PortraitFlipped:
+                    return 270;
+                case DisplayOrientations.Landscape:
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- XamlBrewer.Uwp.ImageCropperSample/Views/CameraPage.xaml.cs
+++ XamlBrewer.Uwp.ImageCropperSample/Views/CameraPage.xaml.cs
@@ -1,64 +1,31 @@
-﻿using System;
+﻿using Mvvm.Services;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Graphics.Imaging;
+using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using XamlBrewer.Uwp.Controls;
-using Windows.Media.Capture;
-using Windows.Media.MediaProperties;
-using Windows.Storage.Streams;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Windows.Devices.Enumeration;
-using Windows.UI.Core;
-using Windows.System.Display;
-using Windows.Graphics.Display;
 
 namespace XamlBrewer.Uwp.ImageCropperSample
 {
     public sealed partial class CameraPage : Page
     {
-        // Receive notifications about rotation of the UI and apply any necessary rotation to the preview stream
-        private readonly DisplayInformation _displayInformation = DisplayInformation.GetForCurrentView();
-        private DisplayOrientations _displayOrientation = DisplayOrientations.Portrait;
-
-        // Rotation metadata to apply to the preview stream (MF_MT_VIDEO_ROTATION)
-        // Reference: http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh868174.aspx
-        private static readonly Guid RotationKey = new Guid("C380465D-2271-428C-9B83-ECEA3B4A85C1");
-
-        // Prevent the screen from sleeping while the camera is running
-        private readonly DisplayRequest _displayRequest = new DisplayRequest();
-
-        // MediaCapture and its state variables
-        private MediaCapture _mediaCapture = null;
-        private bool _isInitialized = false;
-        private bool _isPreviewing = false;
-
-        // Information about the camera device
-        private bool _mirroringPreview = false;
-        private bool _externalCamera = false;
+        private CameraService cameraService;
 
         public CameraPage()
         {
             this.InitializeComponent();
+
+            cameraService = new CameraService(CameraPreview);
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            await this.InitializeCameraAsync();
+            await cameraService.InitializeCameraAsync();
             base.OnNavigatedTo(e);
         }
 
@@ -68,7 +35,7 @@ namespace XamlBrewer.Uwp.ImageCropperSample
             StorageFolder temp = ApplicationData.Current.LocalCacheFolder;
             StorageFile file = await temp.CreateFileAsync("current_image.png", CreationCollisionOption.ReplaceExisting);
 
-            await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), file);
+            await cameraService.MediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), file);
 
             StorageFile file2 = await temp.GetFileAsync("current_image.png");
 
@@ -76,7 +43,7 @@ namespace XamlBrewer.Uwp.ImageCropperSample
             await wb.LoadAsync(file2);
             this.ImageCropper.SourceImage = wb;
 
-            await StopPreviewAsync();
+            await cameraService.StopPreviewAsync();
             this.CameraPreview.Visibility = Visibility.Collapsed;
         }
 
@@ -92,229 +59,6 @@ namespace XamlBrewer.Uwp.ImageCropperSample
             if (file != null)
             {
                 await (this.CroppedImage.Source as WriteableBitmap).SaveAsync(file);
-            }
-        }
-
-        private async Task InitializeCameraAsync()
-        {
-            Debug.WriteLine("InitializeCameraAsync");
-
-            if (_mediaCapture == null)
-            {
-                // Attempt to get the back camera if one is available, but use any camera device if not
-                var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Back);
-
-                if (cameraDevice == null)
-                {
-                    Debug.WriteLine("No camera device found!");
-                    return;
-                }
-
-                // Create MediaCapture and its settings
-                _mediaCapture = new MediaCapture();
-
-                // Register for a notification when something goes wrong
-                _mediaCapture.Failed += MediaCapture_Failed;
-
-                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
-
-                // Initialize MediaCapture
-                try
-                {
-                    await _mediaCapture.InitializeAsync(settings);
-                    _isInitialized = true;
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    Debug.WriteLine("The app was denied access to the camera");
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine("Exception when initializing MediaCapture with {0}: {1}", cameraDevice.Id, ex.ToString());
-                }
-
-                // If initialization succeeded, start the preview
-                if (_isInitialized)
-                {
-                    // Figure out where the camera is located
-                    if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Unknown)
-                    {
-                        // No information on the location of the camera, assume it's an external camera, not integrated on the device
-                        _externalCamera = true;
-                    }
-                    else
-                    {
-                        // Camera is fixed on the device
-                        _externalCamera = false;
-
-                        // Only mirror the preview if the camera is on the front panel
-                        _mirroringPreview = (cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
-                    }
-
-                    await StartPreviewAsync();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Queries the available video capture devices to try and find one mounted on the desired panel
-        /// </summary>
-        /// <param name="desiredPanel">The panel on the device that the desired camera is mounted on</param>
-        /// <returns>A DeviceInformation instance with a reference to the camera mounted on the desired panel if available,
-        ///          any other camera if not, or null if no camera is available.</returns>
-        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
-        {
-            // Get available devices for capturing pictures
-            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
-
-            // Get the desired camera by panel
-            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
-
-            // If there is no device mounted on the desired panel, return the first device found
-            return desiredDevice ?? allVideoDevices.FirstOrDefault();
-        }
-
-        private async void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
-        {
-            Debug.WriteLine("MediaCapture_Failed: (0x{0:X}) {1}", errorEventArgs.Code, errorEventArgs.Message);
-
-            await CleanupCameraAsync();
-
-           // await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => GetPreviewFrameButton.IsEnabled = _isPreviewing);
-        }
-
-        /// <summary>
-        /// Cleans up the camera resources (after stopping the preview if necessary) and unregisters from MediaCapture events
-        /// </summary>
-        /// <returns></returns>
-        private async Task CleanupCameraAsync()
-        {
-            if (_isInitialized)
-            {
-                if (_isPreviewing)
-                {
-                    // The call to stop the preview is included here for completeness, but can be
-                    // safely removed if a call to MediaCapture.Dispose() is being made later,
-                    // as the preview will be automatically stopped at that point
-                    await StopPreviewAsync();
-                }
-
-                _isInitialized = false;
-            }
-
-            if (_mediaCapture != null)
-            {
-                _mediaCapture.Failed -= MediaCapture_Failed;
-                _mediaCapture.Dispose();
-                _mediaCapture = null;
-            }
-        }
-
-        /// <summary>
-        /// Starts the preview and adjusts it for for rotation and mirroring after making a request to keep the screen on and unlocks the UI
-        /// </summary>
-        /// <returns></returns>
-        private async Task StartPreviewAsync()
-        {
-            Debug.WriteLine("StartPreviewAsync");
-
-            // Prevent the device from sleeping while the preview is running
-            _displayRequest.RequestActive();
-
-            // Set the preview source in the UI and mirror it if necessary
-            this.CameraPreview.Source = _mediaCapture;
-            CameraPreview.FlowDirection = _mirroringPreview ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
-
-            // Start the preview
-            try
-            {
-                await _mediaCapture.StartPreviewAsync();
-                _isPreviewing = true;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Exception when starting the preview: {0}", ex.ToString());
-            }
-
-            // Initialize the preview to the current orientation
-            if (_isPreviewing)
-            {
-                await SetPreviewRotationAsync();
-            }
-
-            // Enable / disable the button depending on the preview state
-            //GetPreviewFrameButton.IsEnabled = _isPreviewing;
-        }
-
-        /// <summary>
-        /// Gets the current orientation of the UI in relation to the device and applies a corrective rotation to the preview
-        /// </summary>
-        private async Task SetPreviewRotationAsync()
-        {
-            // Only need to update the orientation if the camera is mounted on the device
-            if (_externalCamera) return;
-
-            // Calculate which way and how far to rotate the preview
-            int rotationDegrees = ConvertDisplayOrientationToDegrees(_displayOrientation);
-
-            // The rotation direction needs to be inverted if the preview is being mirrored
-            if (_mirroringPreview)
-            {
-                rotationDegrees = (360 - rotationDegrees) % 360;
-            }
-
-            // Add rotation metadata to the preview stream to make sure the aspect ratio / dimensions match when rendering and getting preview frames
-            var props = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview);
-            props.Properties.Add(RotationKey, rotationDegrees);
-            await _mediaCapture.SetEncodingPropertiesAsync(MediaStreamType.VideoPreview, props, null);
-        }
-
-        /// <summary>
-        /// Stops the preview and deactivates a display request, to allow the screen to go into power saving modes, and locks the UI
-        /// </summary>
-        /// <returns></returns>
-        private async Task StopPreviewAsync()
-        {
-            try
-            {
-                _isPreviewing = false;
-                await _mediaCapture.StopPreviewAsync();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Exception when stopping the preview: {0}", ex.ToString());
-            }
-
-            // Use the dispatcher because this method is sometimes called from non-UI threads
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                this.CameraPreview.Source = null;
-
-                // Allow the device to sleep now that the preview is stopped
-                _displayRequest.RequestRelease();
-
-                // GetPreviewFrameButton.IsEnabled = _isPreviewing;
-            });
-        }
-
-        /// <summary>
-        /// Converts the given orientation of the app on the screen to the corresponding rotation in degrees
-        /// </summary>
-        /// <param name="orientation">The orientation of the app on the screen</param>
-        /// <returns>An orientation in degrees</returns>
-        private static int ConvertDisplayOrientationToDegrees(DisplayOrientations orientation)
-        {
-            switch (orientation)
-            {
-                case DisplayOrientations.Portrait:
-                    return 90;
-                case DisplayOrientations.LandscapeFlipped:
-                    return 180;
-                case DisplayOrientations.PortraitFlipped:
-                    return 270;
-                case DisplayOrientations.Landscape:
-                default:
-                    return 0;
             }
         }
     }
